96aa92324d12012a8485eeb0ee7eb77e9487fb8e
Jonathan Anguise <jonathan.anguise@protonmail.com>
2023-12-02 09:20:17 +0100

    day1: prefere isdigit method instead of regex

--- day1/main.py
+++ day1/main.py
@@ -4,6 +4,15 @@ def elves_trebuchet_solver(input):
     with open(input) as my_text:
         content = my_text.readlines()
 
-    my_regex1 = r".*?(\d).*(\d).*$"
-    my_regex2 = r"todo"
+    #my_regex1 = r".*?(\d).*(\d).*$"
+    #my_regex2 = r"todo"
+    res=0
     for line in content:
+        numbers_array = [char for char in content if char.isdigit()]
+        
+        res_array = numbers_array[0] + numbers_array[-1]
+        if len(numbers_array)>=1:
+
+
+
+
