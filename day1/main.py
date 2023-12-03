1ad0dee4bc35d8dec6c907b8657ff56f37bad419
Jonathan Anguise <jonathan.anguise@protonmail.com>
2023-12-03 11:53:14 +0100

    day1: adapt code for the part 2, text overlapping

--- day1/main.py
+++ day1/main.py
@@ -1,17 +1,19 @@
 import re
 
-def elves_trebuchet_solver(input):
-    with open(input) as my_text:
+def clean_with_overlap(input)
+    with open(input,"r") as my_text:
         content = my_text.readlines()
+    return content
 
-    #my_regex1 = r".*?(\d).*(\d).*$"
-    #my_regex2 = r"todo"
-    res=0
-    for line in content:
-        numbers_array = [char for char in content if char.isdigit()]
-        
-        res_array = numbers_array[0] + numbers_array[-1]
-        if len(numbers_array)>=1:
+def elves_trebuchet_solver(input):
+        res=0
+        for line in clean_with_overlap(input):
+            numbers_array = "".join(filter(str.isdigit, line))
+            res_str = str(numbers_array[0])+ str(numbers_array[-1])
+            res+=int(res_str)
+    return res
+
+#print(elves_trebuchet_solver("input2"))
 
 
 
